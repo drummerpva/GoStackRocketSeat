6d7f0d0357ad3776bfd3d03a7d86da08e9426c02
Douglas Poma Escritório <douglaspoma@yahoo.com>
2019-06-27 17:55:19 -0300

    Inicio tratamento erros

---
+++ goBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '78ed122018a7fd',
+    pass: '17ba0d4ebb2aaa',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
