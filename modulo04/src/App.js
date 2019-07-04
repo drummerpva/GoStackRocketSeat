9793e8c972d637dc7a02d737f963009fbfe1f8bb
Douglas Poma Escritório <douglaspoma@yahoo.com>
2019-07-04 17:53:37 -0300

    Iniciando ReactJS

---
+++ modulo04/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+import profile from "./assets/pp.jpg";
+
+function App() {
+  return <img width="200" src={profile} alt="" />;
+}
+
+export default App;
