27c3888311556c66437579a3fb5dc89e419e2ed6
Douglas Poma <pia_da_lan@yahoo.com.br>
2019-07-04 00:04:08 -0300

    MAPA

--- mapa.cpp
+++ mapa.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-
+/*
+	Trabalho MAPA de Douglas Poma - Curso ADS - Unicesumar - 52/2019
+*/
 struct livro{
 	int codigo = -1;
 	char autor[50];
@@ -18,25 +20,27 @@ main(){
 	while(menu != 0){
 		fflush(stdin);
 		system("cls");
-		printf("==========> Biblio C - Beta <==========\n");
+		printf("==========> Biblio C - Beta <==========\n\n\n");
+		printf("Menu\n");
 		printf("1 - Inserir Livro\n");
 		printf("2 - Mostrar todos os Livros\n");
 		printf("0 - Encerrar\n");
 		printf("Digite o número da opção desejada e tecle ENTER.:  ");
 		scanf("%d",&menu);
 		fflush(stdin);
-		
-		if(menu > 2 || menu < 0){
+		if(menu == 0 ){
+			continue;
+		} else if(menu > 2 || menu < 0){
 			system("cls");
-			printf("==========> Biblio C - Beta <==========\n");
+			printf("==========> Biblio C - Beta <==========\n\n\n");
 			printf("Opção invalida, tente novamente!\n");
 			system("pause");
 		} else if(menu == 1){
 			fflush(stdin);
 			system("cls");
-			printf("==========> Biblio C - Beta <==========\n");
+			printf("==========> Biblio C - Beta <==========\n\n\n");
 			printf("=========> Cadastro de Livros <========\n");
-			if(!(cod >= 4)){
+			if(!(cod > 4)){
 				printf("Insira o nome do Livro.: ");
 				scanf("%[A-Za-z 0-9]", &livros[cod].nome);
 				fflush(stdin);
@@ -57,14 +61,16 @@ main(){
 		}else if(menu == 2){
 			fflush(stdin);
 			system("cls");
-			printf("==========> Biblio C - Beta <==========\n");
+			printf("==========> Biblio C - Beta <==========\n\n\n");
 			printf("========> Relatório de Livros <========\n");
 			if(cod > 0){
 				
-				for(i = 0; i < 4; i++){
+				for(i = 0; i <= 4; i++){
 					if(livros[i].codigo != -1){
 						printf("%dº Livro:\n", i+1);
-						printf("");
+						printf("	Titulo.: %s\n", livros[i].nome);
+						printf("	Autor.: %s\n", livros[i].autor);
+						printf("	Editora.: %s\n", livros[i].editora);
 					}
 				}
 			}else{
@@ -76,8 +82,8 @@ main(){
 		
 	}
 	system("cls");
-	printf("==========> Biblio C - Beta <==========\n");
-	printf("Obrigado por usar nosso sistema! Tenha um bom dia.\n");
+	printf("==========> Biblio C - Beta <==========\n\n\n\n\n");
+	printf("Obrigado por usar nosso sistema! Tenha um bom dia.\n\n\n\n\n");
 	system("pause");
 	
 	return 0;
